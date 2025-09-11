84ff2afbe8f47cb10626d92f6e19a46f34e52a19
Faizah Ahmed <faizahahmed47@gmail.com>
2025-09-11 23:03:52 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+word=input("enter the word")
+k,l=0,(int)(len(word)/2)
+for i in range(l):
+    n1=i
+    n2=(int)(len(word))-i-1
+    if word[n1]!=word[n2]:
+        print("not palindrome")
+        break
+    else:
+        k+=1
+if(k==l):
+    print("palindrome")
